1d2e50b54c1b06b609036c34eee2cdb1242a4fbd
tlecat <tlete7412@gmail.com>
2024-12-06 16:11:54 +0700

    6/12

--- admin/admin_assign.php
+++ admin/admin_assign.php
@@ -79,14 +79,12 @@ $assignments_result = $stmt->get_result();
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>สั่งงานใหม่</title>
     <link href="../css/sidebar.css" rel="stylesheet">
     <link href="../css/navbar.css" rel="stylesheet">
-    <link href="https://www.ppkhosp.go.th/images/logoppk.png" rel="icon">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -267,7 +265,6 @@ $assignments_result = $stmt->get_result();
                         </button>
                         <div id="selected-users" class="mt-2 text-muted">ยังไม่ได้เลือกผู้ใช้งาน</div>
                     </div>
-
                     <div class="modal fade" id="userModal" tabindex="-1" aria-labelledby="userModalLabel" aria-hidden="true">
                         <div class="modal-dialog modal-lg">
                             <div class="modal-content">
