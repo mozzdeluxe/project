8feec87823c7d66b5a441b93978dd74d75adcdb5
tlecat <tlete7412@gmail.com>
2025-02-18 23:35:20 +0700

    18/02

--- sql/work.sql
+++ sql/work.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 02, 2024 at 09:22 AM
+-- Generation Time: Feb 18, 2025 at 09:19 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -28,27 +28,30 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `assignments` (
-  `assign_id` int(10) NOT NULL,
-  `job_id` int(10) DEFAULT NULL,
-  `supervisor_id` int(11) DEFAULT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `status` enum('pending','completed','late') DEFAULT 'pending',
-  `file_path` varchar(50) NOT NULL
+  `assign_id` int(10) NOT NULL COMMENT 'รหัสการมอบหมาย',
+  `job_id` int(10) DEFAULT NULL COMMENT 'รหัสงาน',
+  `user_id` int(11) DEFAULT NULL COMMENT 'รหัสพนักงาน',
+  `status` enum('ยังไม่อ่าน','อ่านแล้ว','กำลังดำเนินการ','ส่งแล้ว','ช้า') DEFAULT 'ยังไม่อ่าน' COMMENT 'สถานะ',
+  `file_path` varchar(50) NOT NULL COMMENT 'ไฟล์ที่อัป'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Dumping data for table `assignments`
 --
 
-INSERT INTO `assignments` (`assign_id`, `job_id`, `supervisor_id`, `user_id`, `status`, `file_path`) VALUES
-(1, NULL, 4, 3, 'pending', ''),
-(2, NULL, 4, 1, 'late', ''),
-(3, NULL, 2, 1, 'pending', ''),
-(4, NULL, 2, 3, 'pending', ''),
-(5, NULL, 2, 1, 'pending', ''),
-(6, NULL, 2, 1, 'pending', ''),
-(7, NULL, 2, 1, 'pending', ''),
-(8, NULL, 2, 3, 'pending', 'uploads/5DO_5DON_T.pdf');
+INSERT INTO `assignments` (`assign_id`, `job_id`, `user_id`, `status`, `file_path`) VALUES
+(21, 14, 4, 'อ่านแล้ว', 'job14_user3.pdf'),
+(28, 22, 8, 'อ่านแล้ว', 'job22_user3.pdf'),
+(29, 22, 4, 'อ่านแล้ว', 'job22_user3.pdf'),
+(33, 25, 4, '', 'job25_user3.pdf'),
+(34, 25, 7, '', 'job25_user3.pdf'),
+(35, 25, 8, '', 'job25_user3.pdf'),
+(36, 27, 8, 'อ่านแล้ว', 'job27_user3.pdf'),
+(37, 27, 7, 'อ่านแล้ว', 'job27_user3.pdf'),
+(38, 27, 4, 'อ่านแล้ว', 'job27_user3.pdf'),
+(39, 28, 7, 'ยังไม่อ่าน', 'job28_user3.pdf'),
+(40, 29, 7, 'ยังไม่อ่าน', 'job29_user3.pdf'),
+(41, 30, 7, 'ยังไม่อ่าน', 'job30_user3.pdf');
 
 -- --------------------------------------------------------
 
@@ -57,11 +60,10 @@ INSERT INTO `assignments` (`assign_id`, `job_id`, `supervisor_id`, `user_id`, `s
 --
 
 CREATE TABLE `jobs` (
-  `job_id` int(11) NOT NULL COMMENT 'รหัสงาน',
-  `user_id` int(11) NOT NULL COMMENT 'รหัสพนักงาน',
   `supervisor_id` int(11) NOT NULL COMMENT 'รหัสผู้สั่งงาน',
+  `job_id` int(11) NOT NULL COMMENT 'รหัสงาน',
   `job_title` varchar(255) NOT NULL COMMENT 'ชื่อเอกสาร',
-  `job_level` enum('low','medium','high') NOT NULL DEFAULT 'low' COMMENT 'ระดับงาน',
+  `job_level` enum('ปกติ','ด่วน','ด่วนมาก') NOT NULL DEFAULT 'ปกติ' COMMENT 'ระดับงาน',
   `job_description` text NOT NULL COMMENT 'รายละเอียดงาน',
   `due_datetime` datetime NOT NULL COMMENT 'วันและเวลาที่กำหนด',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'วันที่สร้าง',
@@ -73,8 +75,21 @@ CREATE TABLE `jobs` (
 -- Dumping data for table `jobs`
 --
 
-INSERT INTO `jobs` (`job_id`, `user_id`, `supervisor_id`, `job_title`, `job_level`, `job_description`, `due_datetime`, `created_at`, `jobs_file`, `end_date`) VALUES
-(1, 1, 2, 'Report Submission', 'medium', 'Submit monthly reports', '2024-11-25 17:00:00', '2024-11-19 08:38:44', 'report.pdf', '2024-11-26 17:00:00');
+INSERT INTO `jobs` (`supervisor_id`, `job_id`, `job_title`, `job_level`, `job_description`, `due_datetime`, `created_at`, `jobs_file`, `end_date`) VALUES
+(3, 13, 'ertrg', 'ปกติ', 'กหหหด', '2025-01-05 23:59:00', '2025-01-05 17:56:52', '', NULL),
+(3, 14, 'ertrg', 'ปกติ', 'esfd', '2025-01-06 01:04:00', '2025-01-05 18:04:23', 'job14_user3.pdf', NULL),
+(3, 15, 'ๅ/-/ๅ-', 'ปกติ', 'กดเดกเ', '2025-01-06 01:18:00', '2025-01-05 18:18:14', 'job15_user3.pdf', NULL),
+(3, 16, 'หกกเ', 'ปกติ', 'ดเกดเด', '2025-01-06 01:22:00', '2025-01-05 18:22:39', 'job16_user3.pdf', NULL),
+(3, 17, 'ertrg', 'ปกติ', 'ดเิด', '2025-01-06 04:11:00', '2025-01-05 21:11:18', '', NULL),
+(3, 18, 'ertrg', 'ปกติ', 'ดเิด', '2025-01-06 04:11:00', '2025-01-05 21:12:01', '', NULL),
+(3, 19, 'ๅ/-/ๅ-', 'ปกติ', 'กหหแ', '2025-01-06 04:12:00', '2025-01-05 21:12:23', '', NULL),
+(3, 21, 'สวัสดี', 'ปกติ', 'ทดสอบ', '2025-01-06 04:25:00', '2025-01-05 21:25:36', '', NULL),
+(3, 22, 'สวัสดี', 'ปกติ', 'ทดสอบ', '2025-01-06 04:27:00', '2025-01-05 21:28:00', 'job22_user3.pdf', NULL),
+(3, 25, 'mfg', 'ปกติ', 'asdfggh', '2025-01-28 11:53:00', '2025-01-28 04:54:13', 'job25_user3.pdf', NULL),
+(3, 27, 'ertrg', 'ปกติ', 'dfghjkl', '2025-01-28 12:00:00', '2025-01-28 05:00:14', 'job27_user3.pdf', NULL),
+(3, 28, '5555', 'ปกติ', 'กกกก', '2025-02-02 16:05:00', '2025-02-02 09:05:12', 'job28_user3.pdf', NULL),
+(3, 29, '111111111111', 'ปกติ', '1111111111111111', '2025-02-02 16:09:00', '2025-02-02 09:09:43', 'job29_user3.pdf', NULL),
+(3, 30, 'ดอกอ', 'ปกติ', 'กอกดอกด', '2025-02-02 16:12:00', '2025-02-02 09:13:00', 'job30_user3.pdf', NULL);
 
 -- --------------------------------------------------------
 
@@ -101,10 +116,10 @@ CREATE TABLE `mable` (
 --
 
 INSERT INTO `mable` (`id`, `user_id`, `password`, `nametitle`, `firstname`, `lastname`, `phone`, `email`, `userlevel`, `img_path`, `datesave`) VALUES
-(1, 1111, 'hashed_password1', 'นาย', 'John', 'Doe', '0812345678', 'john@example.com', 'm', 'profile1.jpg', '2024-11-19 08:38:44'),
-(2, 2222, 'hashed_password2', 'นาง', 'Jane', 'Smith', '0812345679', 'jane@example.com', 'a', 'profile2.jpg', '2024-11-19 08:38:44'),
 (3, 5555, '6074c6aa3488f3c2dddff2a7ca821aab', '', 'ดลยา', 'บุญครอบ', '2222222222', 'pai.got11@gmail.com', 'a', '', '2024-11-19 08:51:47'),
-(4, 9999, 'fa246d0262c3925617b0c72bb20eeb1d', '', 'yyy', 'uuu', '0666666666', 'thfhty@tgfg.com', 'm', '', '2024-11-19 08:53:23');
+(4, 9999, 'fa246d0262c3925617b0c72bb20eeb1d', '', 'yyy', 'uuu', '0666666666', 'thfhty@tgfg.com', 'm', '', '2024-11-19 08:53:23'),
+(7, 3333, 'b59c67bf196a4758191e42f76670ceba', 'นาย', 'กัลปพฤกษ์', 'มิ่งจันทึก', '0610793221', 'kru1@gmail.com', 'm', '', '2024-12-15 20:12:43'),
+(8, 2222, '934b535800b1cba8f96a5d72f72f1611', 'นาง', 'ffff', 'ffff', '3434434444', 'tharadol@gmail.com', 'm', '', '2024-12-15 20:27:59');
 
 -- --------------------------------------------------------
 
@@ -113,14 +128,26 @@ INSERT INTO `mable` (`id`, `user_id`, `password`, `nametitle`, `firstname`, `las
 --
 
 CREATE TABLE `reply` (
-  `assign_id` int(50) NOT NULL COMMENT 'รหัสการมอบหมาย',
+  `assign_id` int(50) DEFAULT NULL COMMENT 'รหัสการมอบหมาย',
+  `reply_id` int(10) NOT NULL COMMENT 'รหัสตอบกลับ',
   `user_id` int(10) DEFAULT NULL COMMENT 'รหัสพนักงาน',
   `due_datetime` datetime(6) DEFAULT current_timestamp(6) COMMENT 'วันที่เวลากำหนด',
-  `create_at` timestamp(6) NULL DEFAULT current_timestamp(6) COMMENT 'วันที่สร้าง',
+  `create_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6) COMMENT 'วันที่สร้าง',
   `complete_at` datetime(6) DEFAULT current_timestamp(6) COMMENT 'วันที่เสร็จสิ้น',
-  `file_reply` varchar(50) DEFAULT NULL COMMENT 'ไฟล์งานตอบกลับ',
+  `file_reply` varchar(255) DEFAULT NULL COMMENT 'ไฟล์งานตอบกลับ',
   `reply_description` varchar(50) DEFAULT NULL COMMENT 'รายละเอียดงาน'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `reply`
+--
+
+INSERT INTO `reply` (`assign_id`, `reply_id`, `user_id`, `due_datetime`, `create_at`, `complete_at`, `file_reply`, `reply_description`) VALUES
+(NULL, 5, 4, '2025-02-18 12:40:38.000000', '2025-02-17 23:40:38.000000', '2025-02-18 06:40:38.000000', 'uploads/file_67b41d5663cdd8.17088468.pdf', 'ดเกเกด'),
+(NULL, 6, 4, '2025-02-18 12:46:32.000000', '2025-02-17 23:46:32.000000', '2025-02-18 06:46:32.000000', 'uploads/file_67b41eb89768b2.82807903.pdf', 'ดเกเกด'),
+(NULL, 7, 4, '2025-02-18 15:06:46.000000', '2025-02-18 02:06:46.000000', '2025-02-18 09:06:46.000000', 'uploads/file_67b43f969b37f7.21635216.pdf', 'dfgh'),
+(0, 8, 4, '2025-02-18 15:13:23.000000', '2025-02-18 02:13:23.000000', '2025-02-18 09:13:23.000000', 'uploads/file_67b441230b2f90.43466589.pdf', 'dfgh'),
+(NULL, 9, 4, '2025-02-18 15:16:54.000000', '2025-02-18 02:16:54.000000', '2025-02-18 09:16:54.000000', 'uploads/file_67b441f6301433.71437363.pdf', 'dfgh');
 
 --
 -- Indexes for dumped tables
@@ -130,14 +157,14 @@ CREATE TABLE `reply` (
 -- Indexes for table `assignments`
 --
 ALTER TABLE `assignments`
-  ADD PRIMARY KEY (`assign_id`);
+  ADD PRIMARY KEY (`assign_id`),
+  ADD KEY `fk_job_id` (`job_id`);
 
 --
 -- Indexes for table `jobs`
 --
 ALTER TABLE `jobs`
-  ADD PRIMARY KEY (`job_id`),
-  ADD KEY `fk_mable` (`user_id`);
+  ADD PRIMARY KEY (`job_id`) USING BTREE;
 
 --
 -- Indexes for table `mable`
@@ -148,6 +175,13 @@ ALTER TABLE `mable`
   ADD UNIQUE KEY `email` (`email`);
 
 --
+-- Indexes for table `reply`
+--
+ALTER TABLE `reply`
+  ADD PRIMARY KEY (`reply_id`),
+  ADD KEY `fk_assgin_id` (`assign_id`) USING BTREE;
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -155,30 +189,37 @@ ALTER TABLE `mable`
 -- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
-  MODIFY `assign_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `assign_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'รหัสการมอบหมาย', AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `jobs`
 --
 ALTER TABLE `jobs`
-  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสงาน', AUTO_INCREMENT=2;
+  MODIFY `job_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสงาน', AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `mable`
 --
 ALTER TABLE `mable`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ลำดับผู้ใช้', AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'ลำดับผู้ใช้', AUTO_INCREMENT=4446;
+
+--
+-- AUTO_INCREMENT for table `reply`
+--
+ALTER TABLE `reply`
+  MODIFY `reply_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'รหัสตอบกลับ', AUTO_INCREMENT=10;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `jobs`
+-- Constraints for table `assignments`
 --
-ALTER TABLE `jobs`
-  ADD CONSTRAINT `fk_mable` FOREIGN KEY (`user_id`) REFERENCES `mable` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `assignments`
+  ADD CONSTRAINT `fk_job_id` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`job_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
