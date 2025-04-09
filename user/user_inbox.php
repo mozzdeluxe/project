8338e40b98485a983bd307f57c7bb9a7c96263b6
mozzdeluxe <cornnenll0347@gmail.com>
2025-04-09 16:23:01 +0700

    09/04/2568

--- user/user_inbox.php
+++ user/user_inbox.php
@@ -104,9 +104,6 @@ $totalPages = ceil($totalJobs / $limit); // คำนวณจำนวนหน
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>งานที่ได้รับ</title>
-    <link href="../css/sidebar.css" rel="stylesheet">
-    <link href="../css/popup.css" rel="stylesheet">
-    <link href="../css/navbar.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
