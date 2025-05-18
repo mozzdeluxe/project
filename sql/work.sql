dbab0e18cf1792ab0f28cc751c2b6e97d5f1068f
tlecat <tlete7412@gmail.com>
2025-05-18 23:11:34 +0700

    18/5

--- sql/work.sql
+++ sql/work.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 07, 2025 at 07:16 PM
+-- Generation Time: May 13, 2025 at 01:26 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `approved_jobs`
+--
+
+CREATE TABLE `approved_jobs` (
+  `approved_id` int(11) NOT NULL COMMENT 'รหัสงานที่อนุมัติ',
+  `assign_id` int(11) NOT NULL COMMENT 'รหัสการมอบหมาย',
+  `user_id` int(11) NOT NULL COMMENT 'รหัสพนักงาน',
+  `job_id` int(11) NOT NULL COMMENT 'รหัสงาน',
+  `approved_at` datetime DEFAULT current_timestamp() COMMENT 'วันที่อนุมัติ',
+  `approver_id` int(11) NOT NULL COMMENT 'รหัสผู้อนุมัติ',
+  `note` text DEFAULT NULL COMMENT 'หมายเหตุ (ถ้ามี)'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `approved_jobs`
+--
+
+INSERT INTO `approved_jobs` (`approved_id`, `assign_id`, `user_id`, `job_id`, `approved_at`, `approver_id`, `note`) VALUES
+(1, 38, 4, 27, '2025-05-10 16:44:54', 3, NULL),
+(2, 36, 8, 27, '2025-05-11 12:30:56', 3, NULL),
+(3, 38, 4, 27, '2025-05-11 12:31:44', 3, NULL),
+(4, 28, 8, 22, '2025-05-11 12:47:06', 3, NULL),
+(5, 33, 4, 25, '2025-05-11 12:47:26', 3, NULL),
+(6, 29, 4, 22, '2025-05-11 12:47:32', 3, NULL),
+(7, 29, 4, 22, '2025-05-11 12:50:03', 3, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `assignments`
 --
 
@@ -31,7 +60,7 @@ CREATE TABLE `assignments` (
   `assign_id` int(10) NOT NULL COMMENT 'รหัสการมอบหมาย',
   `job_id` int(10) DEFAULT NULL COMMENT 'รหัสงาน',
   `user_id` int(11) DEFAULT NULL COMMENT 'รหัสพนักงาน',
-  `status` enum('ยังไม่อ่าน','อ่านแล้ว','รอตรวจสอบ','เสร็จสิ้้น','ช้า') DEFAULT 'ยังไม่อ่าน' COMMENT 'สถานะ',
+  `status` enum('ยังไม่อ่าน','อ่านแล้ว','รอตรวจสอบ','แก้ไข','รอตรวจแก้ไข','เสร็จสิ้น','ช้า') DEFAULT 'ยังไม่อ่าน' COMMENT 'สถานะ',
   `file_path` varchar(50) NOT NULL COMMENT 'ไฟล์ที่อัป'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
@@ -40,53 +69,18 @@ CREATE TABLE `assignments` (
 --
 
 INSERT INTO `assignments` (`assign_id`, `job_id`, `user_id`, `status`, `file_path`) VALUES
-(21, 14, 4, 'อ่านแล้ว', 'job14_user3.pdf'),
-(28, 22, 8, 'อ่านแล้ว', 'job22_user3.pdf'),
-(29, 22, 4, 'อ่านแล้ว', 'job22_user3.pdf'),
-(33, 25, 4, '', 'job25_user3.pdf'),
-(34, 25, 7, '', 'job25_user3.pdf'),
-(35, 25, 8, '', 'job25_user3.pdf'),
+(21, 14, 4, 'เสร็จสิ้น', 'job14_user3.pdf'),
+(28, 22, 8, 'เสร็จสิ้น', 'job22_user3.pdf'),
+(29, 22, 4, 'เสร็จสิ้น', 'job22_user3.pdf'),
+(33, 25, 4, 'รอตรวจสอบ', 'job25_user3.pdf'),
+(34, 25, 7, 'อ่านแล้ว', 'job25_user3.pdf'),
+(35, 25, 8, 'อ่านแล้ว', 'job25_user3.pdf'),
 (36, 27, 8, 'อ่านแล้ว', 'job27_user3.pdf'),
 (37, 27, 7, 'อ่านแล้ว', 'job27_user3.pdf'),
-(38, 27, 4, 'อ่านแล้ว', 'job27_user3.pdf'),
-(39, 28, 7, 'ยังไม่อ่าน', 'job28_user3.pdf'),
+(38, 27, 4, 'เสร็จสิ้น', 'job27_user3.pdf'),
+(39, 28, 7, '', 'job28_user3.pdf'),
 (40, 29, 7, 'ยังไม่อ่าน', 'job29_user3.pdf'),
 (41, 30, 7, 'ยังไม่อ่าน', 'job30_user3.pdf');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `corrections`
---
-
-CREATE TABLE `corrections` (
-  `correction_id` int(10) NOT NULL COMMENT 'รหัสการส่งกลับแก้ไข',
-  `reply_id` int(10) NOT NULL COMMENT 'รหัสการตอบกลับที่ถูกส่งกลับ',
-  `supervisor_id` int(10) NOT NULL COMMENT 'รหัสหัวหน้างานที่ส่งกลับ',
-  `round` int(5) NOT NULL COMMENT 'รอบที่แก้ไข',
-  `status` enum('รอแก้ไข','แก้ไขแล้ว','อนุมัติ') NOT NULL DEFAULT 'รอแก้ไข' COMMENT 'สถานะการแก้ไข',
-  `comment` text DEFAULT NULL COMMENT 'ความคิดเห็นจากหัวหน้า',
-  `file_correction` varchar(255) DEFAULT NULL COMMENT 'ไฟล์ที่แนบกลับมาแก้ไข',
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() COMMENT 'วันที่ส่งกลับ',
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT 'วันที่อัปเดต'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Triggers `corrections`
---
-DELIMITER $$
-CREATE TRIGGER `update_assignment_status` AFTER INSERT ON `corrections` FOR EACH ROW UPDATE assignments
-SET status = 'รอตรวจสอบ'
-WHERE assign_id = (SELECT assign_id FROM reply WHERE reply_id = NEW.reply_id)
-$$
-DELIMITER ;
-DELIMITER $$
-CREATE TRIGGER `update_assignment_status_on_approval` AFTER UPDATE ON `corrections` FOR EACH ROW UPDATE assignments
-SET status = 'เสร็จสิ้้น'
-WHERE assign_id = (SELECT assign_id FROM reply WHERE reply_id = NEW.reply_id)
-AND NEW.status = 'อนุมัติ'
-$$
-DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -183,14 +177,57 @@ INSERT INTO `reply` (`assign_id`, `reply_id`, `user_id`, `due_datetime`, `create
 (NULL, 7, 4, '2025-02-18 15:06:46.000000', '2025-02-18 02:06:46.000000', '2025-02-18 09:06:46.000000', 'uploads/file_67b43f969b37f7.21635216.pdf', 'dfgh'),
 (0, 8, 4, '2025-02-18 15:13:23.000000', '2025-02-18 02:13:23.000000', '2025-02-18 09:13:23.000000', 'uploads/file_67b441230b2f90.43466589.pdf', 'dfgh'),
 (NULL, 9, 4, '2025-02-18 15:16:54.000000', '2025-02-18 02:16:54.000000', '2025-02-18 09:16:54.000000', 'uploads/file_67b441f6301433.71437363.pdf', 'dfgh'),
-(27, 10, 4, '2025-03-07 16:11:44.000000', '2025-03-07 03:11:44.000000', '2025-03-07 10:11:44.000000', 'uploads/file_67cab850511e96.60355715.pdf', '่้เดกกดเ'),
-(27, 11, 4, '2025-03-07 16:13:36.000000', '2025-03-07 03:13:36.000000', '2025-03-07 10:13:36.000000', 'uploads/file_67cab8c0b83416.78198549.docx', '่้เดกกดเ'),
-(27, 12, 4, '2025-03-07 16:29:10.000000', '2025-03-07 03:29:10.000000', '2025-03-07 10:29:10.000000', 'uploads/file_67cabc662c3a08.24202271.pdf', 'กหอด'),
-(27, 13, 4, '2025-03-07 16:31:57.000000', '2025-03-07 03:31:57.000000', '2025-03-07 10:31:57.000000', 'uploads/file_67cabd0dce55e5.09472775.docx', 'กดกดเ');
+(NULL, 10, 4, '2025-02-18 15:43:01.000000', '2025-02-18 02:43:01.000000', '2025-02-18 09:43:01.000000', 'uploads/file_67b44815571290.16326610.pdf', 'dfgh'),
+(NULL, 11, 8, '2025-02-18 15:43:58.000000', '2025-02-18 02:43:58.000000', '2025-02-18 09:43:58.000000', 'uploads/file_67b4484ec46514.68406463.pdf', 'dscsd'),
+(38, 12, 4, '2025-05-09 15:36:07.000000', '2025-05-09 08:36:07.000000', '2025-05-09 15:36:07.000000', 'uploads/file_681dbe77bcb9b4.51830442.pdf', 'หหหหห'),
+(38, 14, 4, '2025-05-09 15:58:37.000000', '2025-05-09 08:58:37.000000', '2025-05-09 15:58:37.000000', 'uploads/file_681dc3bdc52c32.57695178.pdf', 'aaaaaaaaa'),
+(38, 15, 4, '2025-05-09 16:00:06.000000', '2025-05-09 09:00:06.000000', '2025-05-09 16:00:06.000000', 'uploads/file_681dc416743c73.25298747.docx', 'ไหหหก'),
+(38, 16, 4, '2025-05-09 16:17:06.000000', '2025-05-09 09:17:06.000000', '2025-05-09 16:17:06.000000', 'uploads/file_681dc8124125d5.90071787.pdf', 'ำำำำำำำำำำำำำำำ'),
+(21, 17, 4, '2025-05-09 16:17:37.000000', '2025-05-09 09:17:37.000000', '2025-05-09 16:17:37.000000', 'uploads/file_681dc8313efa48.13538187.pdf', 'qqqqqqqqqqq'),
+(29, 21, 4, '2025-05-11 12:42:34.000000', '2025-05-11 05:42:34.000000', '2025-05-11 12:42:34.000000', 'uploads/file_682038caf087d7.13943736.docx', 'ฟฟฟฟฟฟฟฟฟฟฟฟฟฟ'),
+(29, 23, 4, '2025-05-11 12:46:39.000000', '2025-05-11 05:46:39.000000', '2025-05-11 12:46:39.000000', 'uploads/file_682039bfc05966.57590522.pdf', 'าาาาาาาาาาาาาาาาาา'),
+(25, 24, 4, '2025-05-12 16:15:23.000000', '2025-05-12 09:15:23.000000', '2025-05-12 16:15:23.000000', 'uploads/reply_6821bc2be8a916.83640442.pdf', 'กำกำไก'),
+(25, 25, 4, '2025-05-12 16:29:08.000000', '2025-05-12 09:29:08.000000', '2025-05-12 16:29:08.000000', 'uploads/reply_6821bf64853c40.19309324.docx', 'หปฟปฟห'),
+(25, 26, 4, '2025-05-12 16:37:06.000000', '2025-05-12 09:37:06.000000', '2025-05-12 16:37:06.000000', 'uploads/reply_6821c1420b8583.59822528.pdf', 'หฟปฟหปแก'),
+(33, 28, 4, '2025-05-13 16:40:37.000000', '2025-05-13 09:40:37.000000', '2025-05-13 16:40:37.000000', 'uploads/file_68231395725035.64672435.docx', 'ทดสอบ');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `revisions`
+--
+
+CREATE TABLE `revisions` (
+  `revisions_id` int(11) NOT NULL COMMENT 'รหัสงานรอแก้ไข',
+  `assign_id` int(11) NOT NULL COMMENT 'รหัสการมอบหมาย',
+  `user_id` int(11) NOT NULL COMMENT 'รหัสพนักงาน',
+  `job_id` int(11) NOT NULL COMMENT 'รหัสงาน',
+  `revision_at` datetime DEFAULT current_timestamp() COMMENT 'วันที่สั่งให้แก้ไข',
+  `reviser_id` int(11) NOT NULL COMMENT 'รหัสผู้สั่งให้แก้',
+  `reason` text DEFAULT NULL COMMENT 'เหตุผลในการให้แก้ไข'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `revisions`
+--
+
+INSERT INTO `revisions` (`revisions_id`, `assign_id`, `user_id`, `job_id`, `revision_at`, `reviser_id`, `reason`) VALUES
+(1, 33, 9999, 25, '2025-05-12 14:01:07', 3, 'กกกกก'),
+(2, 33, 9999, 25, '2025-05-12 14:10:25', 3, 'กกกก'),
+(3, 33, 9999, 25, '2025-05-12 14:12:19', 3, 'ssssssss'),
+(4, 33, 9999, 25, '2025-05-13 16:39:33', 3, 'ทดสอบ'),
+(5, 33, 9999, 25, '2025-05-13 16:53:57', 3, 'แก้ครั้งที่2');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `approved_jobs`
+--
+ALTER TABLE `approved_jobs`
+  ADD PRIMARY KEY (`approved_id`),
+  ADD KEY `assign_id` (`assign_id`);
 
 --
 -- Indexes for table `assignments`
@@ -198,14 +235,6 @@ INSERT INTO `reply` (`assign_id`, `reply_id`, `user_id`, `due_datetime`, `create
 ALTER TABLE `assignments`
   ADD PRIMARY KEY (`assign_id`),
   ADD KEY `fk_job_id` (`job_id`);
-
---
--- Indexes for table `corrections`
---
-ALTER TABLE `corrections`
-  ADD PRIMARY KEY (`correction_id`),
-  ADD KEY `reply_id` (`reply_id`),
-  ADD KEY `supervisor_id` (`supervisor_id`);
 
 --
 -- Indexes for table `jobs`
@@ -229,20 +258,27 @@ ALTER TABLE `reply`
   ADD KEY `fk_assgin_id` (`assign_id`) USING BTREE;
 
 --
+-- Indexes for table `revisions`
+--
+ALTER TABLE `revisions`
+  ADD PRIMARY KEY (`revisions_id`),
+  ADD KEY `assign_id` (`assign_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `approved_jobs`
+--
+ALTER TABLE `approved_jobs`
+  MODIFY `approved_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสงานที่อนุมัติ', AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
   MODIFY `assign_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'รหัสการมอบหมาย', AUTO_INCREMENT=42;
-
---
--- AUTO_INCREMENT for table `corrections`
---
-ALTER TABLE `corrections`
-  MODIFY `correction_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'รหัสการส่งกลับแก้ไข';
 
 --
 -- AUTO_INCREMENT for table `jobs`
@@ -260,11 +296,23 @@ ALTER TABLE `mable`
 -- AUTO_INCREMENT for table `reply`
 --
 ALTER TABLE `reply`
-  MODIFY `reply_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'รหัสตอบกลับ', AUTO_INCREMENT=14;
+  MODIFY `reply_id` int(10) NOT NULL AUTO_INCREMENT COMMENT 'รหัสตอบกลับ', AUTO_INCREMENT=29;
+
+--
+-- AUTO_INCREMENT for table `revisions`
+--
+ALTER TABLE `revisions`
+  MODIFY `revisions_id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'รหัสงานรอแก้ไข', AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `approved_jobs`
+--
+ALTER TABLE `approved_jobs`
+  ADD CONSTRAINT `approved_jobs_ibfk_1` FOREIGN KEY (`assign_id`) REFERENCES `assignments` (`assign_id`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `assignments`
@@ -273,11 +321,10 @@ ALTER TABLE `assignments`
   ADD CONSTRAINT `fk_job_id` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`job_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
--- Constraints for table `corrections`
+-- Constraints for table `revisions`
 --
-ALTER TABLE `corrections`
-  ADD CONSTRAINT `corrections_ibfk_1` FOREIGN KEY (`reply_id`) REFERENCES `reply` (`reply_id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `corrections_ibfk_2` FOREIGN KEY (`supervisor_id`) REFERENCES `mable` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `revisions`
+  ADD CONSTRAINT `revisions_ibfk_1` FOREIGN KEY (`assign_id`) REFERENCES `assignments` (`assign_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
